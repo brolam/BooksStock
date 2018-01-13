b8430fa2f29d4e80ba8c39135bc8660327226861
Breno Marques <breno@brolam.com.br>
2018-01-13 16:09:21 -0200

    refactor: TDD - BookStockRequiredFields

--- BooksStock.API.Tests/Repository/RepositoryUnitTest.cs
+++ BooksStock.API.Tests/Repository/RepositoryUnitTest.cs
@@ -119,7 +119,9 @@ namespace BooksStock.API.Tests.Repository
             Assert.ThrowsException<Exception>(
                 () => new BookStock(null, 10), "O campo BookName deve  validar valores vazio!"
             );
-
+            Assert.ThrowsException<Exception>(
+                () => new BookStock("", 10), "O campo BookName deve  validar valores vazio!"
+            );
             Assert.ThrowsException<Exception>(
                 () => new BookStock("One BookStock", -1), "O campo Stock Quantity deve validar valores negativos!"
             );
