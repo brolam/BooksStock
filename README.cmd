a13997b9b9a6f29cb2c9f7b5a94a5dfd9867dc9f
Breno Marques <breno@brolam.com.br>
2018-01-15 12:52:00 -0200

    docs: Added README.cmd

---
+++ README.cmd
@@ -0,0 +1,23 @@
+#  Projeto BooksStock
+Um exemplo de um REST .NET C# e um WepApp desenvolvido em React  
+
+## Cenário 
+Uma livraria da cidade teve um aumento no número de seus exemplares e está com
+um problema para identificar todos os livros que possui em estoque. Para ajudar a livraria foi
+solicitado a você desenvolver uma aplicação para gerenciar estes exemplares.
+
+# Pré-requisitos
+* Visual Studio 2017
+* MongoDB
+* NodeJS
+
+## Instalando e executando
+* Baixar o projeto `git clone https://github.com/brolam/BooksStock.git`
+* Iniciar o MongoDB 
+* Abrir a solução BooksStock.API.sln e executar o projeto BooksStock.API
+* Entrar na pasta BooksStock.App via linha de comandos
+* Instalar as dependências `npm install`
+* Iniciar o servidor `npm start`
+
+## Por favor, assista o vídeo abaixo para ver mais detalhes do ambiente de desenvolvimento e testes:
+[![Demonstração](http://img.youtube.com/vi/RkWkN1tW8kA/0.jpg)](http://www.youtube.com/watch?v=RkWkN1tW8kA)
